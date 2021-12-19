764d812c19116546029ae81b4c6544e17fc091ea
V-Mokhun <mohunmax1408@gmail.com>
2021-12-19 12:15:39 +0200

    header mobile fix

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,29 @@
 import Menu from "../components/Menu/Menu";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderInfo from "../components/Header/HeaderInfo";
 
 const Header = () => {
   const [menuActive, setMenuActive] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header className="header">
+    <header className={`header ${scrolled ? "scroll" : ""}`}>
       <div className="container">
         <div className="header__inner">
           <div className="header__top">
